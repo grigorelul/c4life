ad1351fa1bbe3591ed28ac989c2f5be64c382e1b
Smarandache Grigorie-Theodor <grigorie2002@yahoo.com>
2024-03-09 13:53:04 +0200

    Backend

---
+++ c4life_BE/src/main/java/org/example/c4life_be/C4lifeBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.c4life_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C4lifeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C4lifeBeApplication.class, args);
+	}
+
+}
---
+++ c4life_BE/src/test/java/org/example/c4life_be/C4lifeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.c4life_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C4lifeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
